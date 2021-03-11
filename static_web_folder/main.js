d2d322792da062c101503e96c2f32bd11da43417
devnithw <devnithw@gmail.com>
2021-03-11 19:45:38 +0530

    Initialized the eel configuration and imported modules

---
+++ static_web_folder/main.js
@@ -0,0 +1,43 @@
+music_name = "music.mp3"
+let play_btn = document.querySelector("#play");
+let prev_btn = document.querySelector("#pre");
+let next_btn = document.querySelector("#next");
+let isPlaying = false;
+let song = new Audio();
+window.onload = playSong;
+
+function playSong(){
+    song.src = music_name;
+    console.log(song)
+    
+    
+    play_btn.addEventListener('click',function(){
+        if(!isPlaying){
+            song.play();
+            isPlaying = true;
+            document.getElementById("playpause").className = "fas fa-pause fa-lg";
+        }
+        else{
+            song.pause();
+            isPlaying = false;
+            document.getElementById("playpause").className = "fas fa-play fa-lg";
+        }
+        song.addEventListener('ended',function(){
+            song.pause();
+            isPlaying = false;
+            document.getElementById("playpause").className = "fas fa-play fa-lg";
+        })
+        
+    
+    })
+}
+
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  
+  /* Set the width of the side navigation to 0 */
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
